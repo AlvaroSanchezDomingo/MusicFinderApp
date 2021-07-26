e4b6e68b9b2a7451743e8f9bc1460711b7c47ac7
AlvaroSanchezDomingo <alvaro.sanchez.domingo@gmail.com>
2021-07-26 20:29:18 +0200

    Search tracks based on text sugestion and show in a list

--- src/api/shazan-api.js
+++ src/api/shazan-api.js
@@ -13,23 +13,21 @@ export const autoComplete = async (searchText) => {
   return response.json();
   
 };
-export const autoComplete2 = async (searchText) => {
+
+export const search = async (searchText) => {
   let codedText = searchText.replace(/ /g, "%20");
-  fetch("https://shazam.p.rapidapi.com/auto-complete?term="+codedText+"&locale=en-US", {
+  let page = 0;
+  let limit = 10;
+  const response = await fetch("https://shazam.p.rapidapi.com/search?term="+codedText+"&locale=en-US&offset="+page+"&limit="+limit, {
     "method": "GET",
     "headers": {
       "x-rapidapi-key": "752e5c2b77mshd527e35bbfb1033p1b415bjsna2d20660dbea",
       "x-rapidapi-host": "shazam.p.rapidapi.com"
     }
-  })
-  .then((response) => response.json())
-  .then((result) => {
-    console.log(result)
-    return result;
-  })
-  .catch(err => {
-    console.error(err);
   });
+  if (!response.ok) {
+    throw new Error(response.json().message);
+  }
+  return response.json();
   
 };
-//Make the api async await to return values syncronized
--- src/components/templateMusicListPage/index.js
+++ src/components/templateMusicListPage/index.js
@@ -1,3 +1,4 @@
+import { search } from "../../api/shazan-api";
 import React, { useState } from "react";
 import MusicSuggestionCard from "../musicSuggestionCard";
 import SampleTrack from "./sampleTrack";
@@ -13,17 +14,22 @@ const useStyles = makeStyles({
 
 function TrackListPageTemplate() {
   const classes = useStyles();
-  const [chosenSuggestion, setChosenSuggestion] = useState("");
-  const tracks = [
-    { ...SampleTrack, id: 1 },
-    { ...SampleTrack, id: 2 },
-    { ...SampleTrack, id: 3 },
-    { ...SampleTrack, id: 4 },
-    { ...SampleTrack, id: 5 },
-  ];
+  const [tracks, setTacks] = useState([{track:
+                                          {
+                                            key:20066955,
+                                            title:"Kiss The Rain",
+                                            subtitle:"Billie Myers",
+                                            images:{
+                                              background:"https://is5-ssl.mzstatic.com/image/thumb/Features124/v4/4e/0d/1f/4e0d1ff5-9f2e-0170-0ec9-3dbe25f3a471/pr_source.png/800x800cc.jpg"
+                                            },
+                                            url:"https://www.shazam.com/track/20066955/kiss-the-rain"
+                                          }
+                                        }]);
 
-  const handleChange = (value) => {
-    setChosenSuggestion(value);
+  const handleChange = async (value) => {
+    await search(value).then(result => {
+      setTacks(result.tracks.hits);
+    });
   };
 
   return (
@@ -34,7 +40,6 @@ function TrackListPageTemplate() {
             onUserInput={handleChange}
           />
         </Grid>
-        {chosenSuggestion}
         <TrackList tracks={tracks}></TrackList>
       </Grid>
     </Grid>
